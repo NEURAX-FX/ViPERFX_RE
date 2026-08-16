499b1cac6f59e55803bcf439e1fe616795dfa1af
NEURAX08 <neurax08@gmail.com>
2026-08-15 20:15:56 -0400

    Revert "feat(module): pin ViPER to the music output session"
    This reverts commit 623367472602693488ef634c9b817d7ad5dcea86.

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -1,32 +1,20 @@
-#!/system/bin/sh
-
-. "$MODPATH/common/session0-pinner.sh"
-
-LIBPATCH="$(cat "$MODPATH/libpatch.txt")"
-CFGS="$(find /odm /system /vendor -type f \( -name '*audio_effects*.conf' -o -name '*audio_effects*.xml' \) 2>/dev/null)"
-
-for FILE in $CFGS; do
-  BACKUP="${FILE}.viper-session0-backup"
-  cp -f "$FILE" "$BACKUP" || continue
-  case "$FILE" in
+LIBPATCH=`cat $MODPATH/libpatch.txt`
+CFGS="$(find /odm /system /vendor -type f -name "*audio_effects*.conf" -o -name "*audio_effects*.xml")"
+for FILE in ${CFGS}; do
+  case $FILE in
     *.conf)
-      sed -i "/v4a_standard_re {/,/}/d" "$FILE"
-      sed -i "/v4a_re {/,/}/d" "$FILE"
-      sed -i "s|^effects {|effects {\n  v4a_standard_re {\n    library v4a_re\n    uuid 90380da3-8536-4744-a6a3-5731970e640f\n  }|g" "$FILE"
-      sed -i "s|^libraries {|libraries {\n  v4a_re {\n    path $LIBPATCH/lib/soundfx/libv4a_re.so\n  }|g" "$FILE"
-      ;;
-    *.xml)
-      sed -i "/<effect name=\"v4a_standard_re\"/d" "$FILE"
-      sed -i "/<library name=\"v4a_re\"/d" "$FILE"
-      sed -i "/<libraries>/ a\        <library name=\"v4a_re\" path=\"libv4a_re.so\"\/>" "$FILE"
-      sed -i "/<effects>/ a\        <effect name=\"v4a_standard_re\" library=\"v4a_re\" uuid=\"90380da3-8536-4744-a6a3-5731970e640f\"\/>" "$FILE"
-      ;;
+        sed -i "/v4a_standard_re {/,/}/d" $FILE
+        sed -i "/v4a_re {/,/}/d" $FILE
+        sed -i "s/^effects {/effects {\n  v4a_standard_re {\n    library v4a_re\n    uuid 90380da3-8536-4744-a6a3-5731970e640f\n  }/g" $FILE
+        sed -i "s/^libraries {/libraries {\n  v4a_re {\n    path $LIBPATCH\/lib\/soundfx\/libv4a_re.so\n  }/g" $FILE
+        ;;
+    *.xml) 
+        sed -i "/v4a_standard_re/d" $FILE
+        sed -i "/v4a_re/d" $FILE
+        sed -i "/<libraries>/ a\        <library name=\"v4a_re\" path=\"libv4a_re.so\"\/>" $FILE
+        sed -i "/<effects>/ a\        <effect name=\"v4a_standard_re\" library=\"v4a_re\" uuid=\"90380da3-8536-4744-a6a3-5731970e640f\"\/>" $FILE
+        ;;
   esac
-  if ! pin_viper_session0_effect "$FILE" || ! validate_viper_session0_effect "$FILE"; then
-    cp -f "$BACKUP" "$FILE"
-    echo "ViPER: skipped invalid session 0 patch for $FILE"
-  fi
-  rm -f "$BACKUP"
 done
 
 if [ -d "/odm/etc/" ]; then
